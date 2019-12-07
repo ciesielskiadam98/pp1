ff3da89a5ff695c0f0f5038fac332f4ffa739b65
Adam Ciesielski <ciesielskiadam1998@gmail.com>
2019-12-07 22:47:03 +0100

    new files added

--- 06-ClassesAndObjects/z21.py
+++ 06-ClassesAndObjects/z21.py
@@ -1,8 +1,6 @@
 class Statystyka():
     def __init__(self, coll):
         self.coll = coll
-        self.max1 = max(self.coll)
-        self.min1 = min(self.coll)
     def add_number(self):
         self.coll.append(int(input('Dodaj liczbę do zbioru: ')))
     def show(self):
@@ -10,21 +8,24 @@ class Statystyka():
             print(i, end = ' ')
     def maxx(self):
         self.max1 = max(self.coll)
-        print(self.max1)
     def minn(self):
         self.min1 = min(self.coll)
-        print(self.min1)
     def avg(self):
         suma = 0
         for i in self.coll:
             suma += i
-        print(suma/len(self.coll))
+        self.avgg = suma / len(self.coll)
+    def display(self):
+        print(self.max1)
+        print(self.min1)
+        print(self.avgg)
         
+    
         
         
 num1 = Statystyka([12, 37, 6, 9, 17])
 num1.maxx()
 num1.minn()
 num1.avg()
-
+num1.display()
 
